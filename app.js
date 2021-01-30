cc4e009e5993d4f4e9783139c45f6cae4894e6a0
Med Amine <76520932+Amine8782@users.noreply.github.com>
2021-01-30 18:32:53 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+let x = document.getElementById('Connecter');
+let y = document.getElementById('Regidter');
+let z = document.getElementById('btn');
+
+function Register() {
+    x.style.left = '-400px';
+    y.style.left = '50px';
+    z.style.left = '110px';
+  }
+
+function Connecter() {
+    x.style.left = '50px';
+    y.style.left = '450px';
+    z.style.left = '0px';
+  }
